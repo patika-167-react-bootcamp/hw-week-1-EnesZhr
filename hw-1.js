903f222ba604398add1162c27fe90d637189a87b
EnesZhr <enes.zehir@hotmail.com>
2022-02-09 10:45:10 +0300

    hw-1 add copy function

--- hw-1.js
+++ hw-1.js
@@ -58,7 +58,7 @@ function move(fileId,folderId){
     //delete first file location
     fileNum.files.splice(indexNum,1);
 
-    return console.log("Added Successfully");
+    return console.log("Move Successfully");
 
   }else{
     //check if fileNum and folderNum are in the folders array
@@ -79,8 +79,62 @@ function move(fileId,folderId){
 }
 
 
+function copy(fileId, folderId){
+  if(typeof fileId === "string" || typeof folderId === "string"){
+    //check paramaters of number
+    console.log("Parameters are not strings, please change the parameters to numbers!!!");
+
+  }else{
+    //file and folder 
+    const fileNum = folders.find((x) => x.files !== undefined && x.files.find((y) => y.id === fileId))
+    const folderNum = folders.find(x => x.id === folderId)
+
+  if(fileNum !== undefined && folderNum !== undefined){
+    //file index number
+    const indexNum = fileNum.files.findIndex(x => x.id === fileId)
+
+    if(folderNum.files !== undefined){
+      //check folder has same file id 
+      if(folderNum.files.some(x => x.id === fileId )){
+
+        return console.log("The file you are trying to add already exists here");
+
+      }else{
+        //folder has files array add target file item
+        folderNum.files.push(fileNum.files[indexNum])
+
+      }
+
+    }else{
+      //folder has not files array create and add target file item
+      folderNum.files = [];
+      folderNum.files.push(fileNum.files[indexNum])
+
+    }
+
+    return console.log("Copy Successfully");
+
+   }else{
+    //check if fileNum and folderNum are in the folders array
+    if(fileNum === undefined && folderNum === undefined){
+
+      console.log("File and Folder id not found");
+
+    }else if( folderNum === undefined){
+
+      console.log("Folder id not found");
+
+    }else{
+
+      console.log("File id not found");
+    }
+   }
+  }
+}
+
+
 move(17,7) // dosyayı klasöre taşıyacak
-// copy(18,7) // kopyasını oluşturacak
+copy(18,7) // kopyasını oluşturacak
 // remove(17) // dosyayı silecek
 // removeFolder(6) //klasörü ve altındaki tüm dosyaları silecek
 // parentFolderOf(17) // ==> 5
